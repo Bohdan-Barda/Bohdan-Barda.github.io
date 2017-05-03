ecb2429a7d561d30b012954130cdddaf97a7ab1b
Bohdan-Barda <Bohdbard@gmail.com>
2017-05-03 19:22:41 +0300

    Update scripts.js

--- pizzeria/js/scripts.js
+++ pizzeria/js/scripts.js
@@ -133,3 +133,10 @@ $(function () {
         }
     })
 });
+/*===preloader-scripts===*/
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
