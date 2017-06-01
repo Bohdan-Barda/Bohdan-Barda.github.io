8286993a9e9c0e447b710297ab444d37aa813f8c
Bohdan-Barda <Bohdbard@gmail.com>
2017-06-01 11:59:46 +0300

    Update scripts.js

--- pizzeria/js/scripts.js
+++ pizzeria/js/scripts.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
 $(function () {
     $('.click__pizza , .click__sushi , .click__ice__cream , .click__drink').on('click', function (e) {
         $('html,body').stop().animate({
-            scrollTop: $('.check__menu').offset().top
+            scrollTop: $('.pizza__menu__h2').offset().top
         }, 1000);
         e.preventDefault();
     });
